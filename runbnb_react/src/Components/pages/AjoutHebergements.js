f7c5a3ab60fb2f3564f657d554a325f0ff7a8de7
chris-m11 <nartha.chris@gmail.com>
2020-10-20 17:51:56 +0200

    v5

--- runbnb_react/src/Components/pages/AjoutHebergements.js
+++ runbnb_react/src/Components/pages/AjoutHebergements.js
@@ -31,13 +31,9 @@ import PageCharte from '../Logements/PageCharte';
 
 
 export default function CustomizedSteppers() {
-<<<<<<< HEAD
-  const [activeStep, setActiveStep] = React.useState(4);
-=======
 
   const [activeStep, setActiveStep] = React.useState(5);
 
->>>>>>> a89ffb6e69606fca43e9b81e2afa97781617057b
   const [formValue, setFormValue] = React.useState({
     hebergement: {
       name: '',
