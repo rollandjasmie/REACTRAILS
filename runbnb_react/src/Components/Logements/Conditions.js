e3c1d51050abef63710df7d813b2bd3389f488ce
iantsaraz <iantsaraz02@gmail.com>
2020-10-21 17:15:36 +0300

    boutton calendrier

--- runbnb_react/src/Components/Logements/Conditions.js
+++ runbnb_react/src/Components/Logements/Conditions.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Formik, Field, Form } from 'formik';
+import '../../App.css';
 
 class Conditions extends React.Component {
   render() {
     return (
-
-     < div className="w-auto on inline-block element h-200 pl-10 ml-10 mt-20  bg-white shadow-md rounded px-8 pt-20 pb-8 mb-10">
+      <>
+       <p className="text-2ml font-bold  px-5 py-5 ">Quels équipements proposez-vous ?</p>
+     < div className="w-2/4 on inline-block element h-200 pl-10 ml-10   bg-white shadow-md rounded px-8 pt-20 ">
+     <p className="text-2ml  px-2  "><span className="uppercase">à</span> quelle dates les clients peuvent-ils annuler leur réservations ?</p>
         <Formik
         initialValues={{
           conditions: '',
@@ -24,32 +27,36 @@ class Conditions extends React.Component {
        }}
         >
           <Form>
-            <div role="group" >
-              <label>
-                <Field type="radio" name="conditions" value="1 jours" />
+            <div className="w-full flex py-5" >
+              <label className="² w-1/2  py-2 border border-orange-500 text-orange-500 text-center hover:bg-orange-500
+               hover:text-white hover:font-bold" >
+                <Field className="hidden" type="radio" name="conditions" value="1 jours" />
                 1 jours
               </label>
-              <label>
-                <Field type="radio" name="conditions" value="1 jours" />
+              <label className="² w-1/2  py-2 border border-orange-500 text-orange-500 text-center hover:bg-orange-500 
+              hover:text-white hover:font-bold">
+                <Field className="hidden" type="radio" name="conditions" value="1 jours" />
                 7 jours
               </label>
-              <label>
-                <Field type="radio" name="conditions" value="1 jours" />
+              <label className="² w-1/2  py-2 border border-orange-500 text-orange-500 text-center hover:bg-orange-500 
+              hover:text-white hover:font-bold">
+                <Field className="hidden" type="radio" name="conditions" value="1 jours" />
                 14 jours
               </label>
-              <label>
-                <Field type="radio" name="conditions" value="1 jours" />
+              <label className="² w-1/2  py-2 border border-orange-500 text-orange-500 text-center hover:bg-orange-500
+               hover:text-white hover:font-bold">
+                <Field className="hidden" type="radio" name="conditions" value="1 jours" />
                 30 jours
               </label>
             </div>
-            <div className="flex items-end justify-end">
+            <div className="flex items-end justify-end my-5">
               <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mr-1" onClick={this.props.previousStep}>Précedent</button>
               <button  class="bg-orange-500 hover:bg-orange-700 text-white pl-6 pr-6 font-bold py-2 px-4 rounded" type="submit">Suivant</button>
             </div>
           </Form>
         </Formik>
         </div>
-        
+       </> 
     )
   }
 }
