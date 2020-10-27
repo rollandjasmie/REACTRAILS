bdf79d072582738796bd0411b43c68d8bc01715d
chris-m11 <nartha.chris@gmail.com>
2020-10-27 09:24:55 +0100

    dash

--- runbnb_react/src/Components/user/EditProfil.js
+++ runbnb_react/src/Components/user/EditProfil.js
@@ -1,2 +1,15 @@
+import React, { Component } from 'react'
+class EditProfile extends Component {
+    render() {
+        return (
+            <>
+                 
+   
 
+
+            </>
+        )
+    }
+}
+export default EditProfile;
 
