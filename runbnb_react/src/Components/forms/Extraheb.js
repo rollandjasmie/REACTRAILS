2cc7f75248126cb287c623cbc0b6c9f6f5fb7441
iantsaraz <iantsaraz02@gmail.com>
2020-10-29 15:49:44 +0300

    suppr

--- runbnb_react/src/Components/forms/Extraheb.js
+++ runbnb_react/src/Components/forms/Extraheb.js
@@ -13,9 +13,6 @@ class Details extends React.Component{
         <Formik>
             <div className="w-full bg-white">
                 <Navbarextra/>
-               
-                
-              
                         <h1 className="tracking-wide text-gray-700 mx-3 text-lg font-bold my-3"> > Détails de l'hébergement</h1>
                   
                 
