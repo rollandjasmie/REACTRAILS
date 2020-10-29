e2cb66a25558dcb447e7a62c8e7c6617e47fb4d3
safidypatrick <safidypatrick8gmail.com>
2020-10-29 17:03:58 +0300

    back_logemant

--- runbnbrails/db/schema.rb
+++ runbnbrails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_104736) do
+ActiveRecord::Schema.define(version: 2020_10_29_133711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 2020_10_29_104736) do
     t.index ["logement_id"], name: "index_adresses_on_logement_id"
   end
 
+  create_table "autres", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "calendriers", force: :cascade do |t|
     t.datetime "startDate"
     t.datetime "endDate"
@@ -33,6 +38,11 @@ ActiveRecord::Schema.define(version: 2020_10_29_104736) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["logement_id"], name: "index_calendriers_on_logement_id"
+  end
+
+  create_table "chambres", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "conditions", force: :cascade do |t|
@@ -78,6 +88,11 @@ ActiveRecord::Schema.define(version: 2020_10_29_104736) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["logement_id"], name: "index_regles_on_logement_id"
+  end
+
+  create_table "salons", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
