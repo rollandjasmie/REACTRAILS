d0f4a22debc16187f42cac5463ec4bf083552fec
rollandjasmie <raharimalalarolland@gmailcom>
2020-11-04 09:19:59 +0300

    error inscription

--- runbnb_react/src/Components/forms/SignIn.js
+++ runbnb_react/src/Components/forms/SignIn.js
@@ -46,8 +46,10 @@ class SignIn extends Component {
     };
 
     render() {
+      const today = new Date();
         const {first_name,name,date_of_birth, email, password,mobile} = this.state
     return (
+      
         <>
            <Navbarin/>
             <div className="flex bg-white">
@@ -63,30 +65,30 @@ class SignIn extends Component {
                         <label className="block tracking-wide text-gray-700 text-base font-bold mb-2 my-2" for="grid-city" >
                          Nom
                         </label>
-                        <input className=" w-full  appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
+                        <input required className=" w-full  appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
                          px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Nom"onChange={this.handleChange}   name="name" value={name}/>
 
                         <label className="block tracking-wide text-gray-700 text-base font-bold mb-2 my-8" for="grid-city" >
                        Prénom
                         </label>
-                        <input className="w-full appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
+                        <input required className="w-full appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
                          px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Prénom "onChange={this.handleChange}    name="first_name"  value = {first_name}/>
 
                         <label className="block tracking-wide text-gray-700 text-base font-bold mb-2 my-8" for="grid-city">
                          Adresse e-mail
                         </label>
-                        <input className="w-full appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
+                        <input required type="email" className="w-full appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
                          px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Adresse e-mail "onChange={this.handleChange}   name="email" value={email}/>
                          <label className="block tracking-wide text-gray-500 text-xs my-3 " for="grid-city">
                          Entrez une adresse e-mail valide
                         </label>
-                        <label className="block tracking-wide text-red-500 text-xs my-3 " for="grid-city">
+                        <label  className="block tracking-wide text-red-500 text-xs my-3 " for="grid-city">
                          {this.state.erros}
                         </label>
-                        <label className="block tracking-wide text-gray-700 text-base font-bold mb-2 my-8" for="grid-city" >
+                        <label  className="block tracking-wide text-gray-700 text-base font-bold mb-2 my-8" for="grid-city" >
                         Numéro de téléphone
                         </label>
-                        <input  className="w-full appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
+                        <input required type="tel" className="w-full appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
                          px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Téléphone "onChange={this.handleChange}   name="mobile" value={mobile} />
                            <label className="block tracking-wide text-gray-500 text-xs my-3 " for="grid-city">
                            Entrez une numéro de téléphone valide
@@ -95,13 +97,13 @@ class SignIn extends Component {
                         <label className="block tracking-wide text-gray-700 text-base font-bold mb-2 my-8" for="grid-city">
                         Date de naissance 
                         </label>
-                        <input  className="w-full appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
+                        <input required type="date" min="1940-01-01" max="2022-01-02" className="w-full appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
                          px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Date de naissance "onChange={this.handleChange}    name="date_of_birth" value={date_of_birth} />
 
                         <label className="block tracking-wide text-gray-700 text-base font-bold mb-2 my-8" for="grid-city">
                         Mot de passe 
                         </label>
-                        <input  type="password" className="w-full appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
+                        <input required  type="password" className="w-full appearance-none block  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3
                          px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" onChange={this.handleChange}  placeholder=" Mot de passe  " name="password" value={password}/>                                                                                  
 
                         <button className="w-full appearance-none block  bg-orange-500 text-white rounded py-3
